687926ae1171d75ea7d78ba4e2db486c9063d197
sterafaa <senatovicvova2@gmail.com>
2026-07-01 23:46:59 +0300

    Hide secret key using env variable

--- date_project/settings.py
+++ date_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nezhcd_g30eunghjh1)g&5gf=yk6b@xe!4u*sxr^$sxp)hzj_0'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-nezhcd_g30eunghjh1)g&5gf=yk6b@xe!4u*sxr^$sxp)hzj_0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
