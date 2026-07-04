5ccbe066928ba00d79eac9400ad4f2cd2eb3b8d1
sterafaa <senatovicvova2@gmail.com>
2026-07-04 13:14:12 +0300

    update

--- invitation/views.py
+++ invitation/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 from .models import Meeting
+from django.http import HttpResponse
 
 def index(request):
     return render(request, "invitation/index.html")
+
 def date(request):
     if request.method == "POST":
+        print("POST")
+        print(request.POST)
+
         meeting_date = request.POST.get("date")
+        print(meeting_date)
 
         if meeting_date:
             Meeting.objects.create(date=meeting_date)
-            
-        return render(request, "invitation/date.html", {
-            "success": True
-        })
+
+        return HttpResponse("OK")
+
     return render(request, "invitation/date.html")
 
 
